b85d35e42070edc5e640a1b8bdfded066fb1b289
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2024-01-11 17:48:28 +0200

    Refactor: unified products data shape

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,26 +5,15 @@ import { themeSettings } from "theme";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
-import { useDispatch, useSelector } from "react-redux";
-import { setProductsData } from "controllers/ProductsReducer.mjs";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import AddProduct from "scenes/add_product";
-import axios from "axios";
 
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    axios.get("http://localhost/")
-      .then((res) => {
-        console.log(res)
-        dispatch(setProductsData(res.data));
-      });
-  }, [dispatch]);
 
   return (
     <div className="app">
--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -1,48 +1,80 @@
-import React from 'react';
-import { Checkbox, FormControlLabel, FormGroup, Typography, useTheme } from '@mui/material';
+import React from "react";
+import {
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Typography,
+  useTheme,
+} from "@mui/material";
 
 function ProductItem({ item, onCheckboxChange }) {
   const theme = useTheme();
 
-  let attributes = '';
+  let attributes = "";
 
-
-  if(item.unique_attribute){
-    attributes = item.unique_attribute
-  }else {
-    if (item.attributes.length == 1) {
-      attributes = item.attributes.map((attribute) => attribute.name + ' : ' + attribute.value).join(', ');
-    } else {
-      attributes = "WxHxL : " + item.attributes.map((attribute) => attribute.value);
-    }
+  if (item.attributes.length == 1) {
+    attributes = item.attributes
+      .map((attribute) => attribute.name + " : " + attribute.value)
+      .join(", ");
+  } else {
+    attributes =
+      "WxHxL : " + item.attributes.map((attribute) => attribute.value);
   }
-  
 
   return (
     <div>
       <FormGroup
         sx={{
-          display: 'flex',
+          display: "flex",
           bgcolor: theme.palette.grey[100],
-          position: 'relative',
-          padding: '1.5rem',
-          width: '14rem',
-          height: '11rem',
+          position: "relative",
+          padding: "1.5rem",
+          width: "14rem",
+          height: "11rem",
           border: `2px solid ${theme.palette.secondary[500]}`,
-          marginTop: '1.5rem',
+          marginTop: "1.5rem",
         }}
       >
         <FormControlLabel
-          sx={{ position: 'absolute', top: 8, left: 13 }}
-          control={<Checkbox className='delete-checkbox' onChange={onCheckboxChange}
-          color="primary" sx={{ width: '100%', height: '100%', color: theme.palette.primary[500] }} />}
+          sx={{ position: "absolute", top: 8, left: 13 }}
+          control={
+            <Checkbox
+              className="delete-checkbox"
+              onChange={onCheckboxChange}
+              color="primary"
+              sx={{
+                width: "100%",
+                height: "100%",
+                color: theme.palette.primary[500],
+              }}
+            />
+          }
           label=""
         />
-        <div style={{ flex: 1, marginLeft: '1rem', marginTop: '1.2rem' }}>
-          <Typography sx={{ color: theme.palette.primary[500], textAlign: 'center' }}> {item.sku}</Typography>
-          <Typography sx={{ color: theme.palette.primary[500], textAlign: 'center' }}> {item.name}</Typography>
-          <Typography sx={{ color: theme.palette.primary[500], textAlign: 'center' }}> {item.price}</Typography>
-          <Typography sx={{ color: theme.palette.primary[500], textAlign: 'center' }}>{attributes}</Typography>
+        <div style={{ flex: 1, marginLeft: "1rem", marginTop: "1.2rem" }}>
+          <Typography
+            sx={{ color: theme.palette.primary[500], textAlign: "center" }}
+          >
+            {" "}
+            {item.sku}
+          </Typography>
+          <Typography
+            sx={{ color: theme.palette.primary[500], textAlign: "center" }}
+          >
+            {" "}
+            {item.name}
+          </Typography>
+          <Typography
+            sx={{ color: theme.palette.primary[500], textAlign: "center" }}
+          >
+            {" "}
+            {item.price}
+          </Typography>
+          <Typography
+            sx={{ color: theme.palette.primary[500], textAlign: "center" }}
+          >
+            {attributes}
+          </Typography>
         </div>
       </FormGroup>
     </div>
