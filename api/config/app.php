9c49b7b83ac32d9b0fc4466eb48b13fc36ac9693
Kareem Hussen <KareemHussen500@gmail.com>
2024-01-12 17:19:53 +0200

    Changing MYSQL port

--- api/config/app.php
+++ api/config/app.php
@@ -6,7 +6,7 @@ define('DB_HOST', 'mysql');
 // For local Development
 // define('DB_HOST', 'localhost');
 
-define('DB_PORT', '3306');
+define('DB_PORT', '3307');
 define('DB_NAME', 'scandiweb_junior_test');
 define('DB_USER', 'php_docker');
 define('DB_PASS', 'password');
